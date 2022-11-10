69bdf170b2214633b0ed8072501fe30c0436db76
konfani <112683626+konfani@users.noreply.github.com>
2022-11-10 03:00:23 +0000

    i forgor to remove debug code

--- src/commands/etc.py
+++ src/commands/etc.py
@@ -1,13 +1,17 @@
 from modules import twitch
 from random import randint
 from twitchio.ext import commands
+from modules import firebase
 
 
 async def russian_roulette(ctx: commands.Context):
-    if randint(1, 1) == 1:
-        await ctx.send("탕!")
+    if randint(1, 6) == 1:
+        await ctx.send(f"탕! {firebase.get_combo(ctx.author.id)}번 살아남으셨습니다!")
+        firebase.set_combo(ctx.author.id, 0)
         twitch.ban(
             ctx.author.id, twitch.username_to_uid(ctx.channel.name), 10, "러시안 룰렛당해버린"
         )
     else:
-        await ctx.send("찰캌")
+        t = firebase.get_combo(ctx.author.id)
+        firebase.set_combo(ctx.author.id, t + 1)
+        await ctx.send(f"찰캌! {t+1}번 살아남으셨습니다!")
