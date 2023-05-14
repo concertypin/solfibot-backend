1ecb5d367adde5edca270528aeb56a41b0cf3dea
concertypin <55056558+concertypin@users.noreply.github.com>
2023-05-14 16:49:31 +0900

    refined somethings

--- src/main/kotlin/dao/databaseFactory.kt
+++ src/main/kotlin/dao/databaseFactory.kt
@@ -2,6 +2,7 @@ package dao
 
 import com.mongodb.reactivestreams.client.MongoDatabase
 import org.litote.kmongo.reactivestreams.KMongo
+import settings.mongoDBconnectionURL
 
-val client = KMongo.createClient()
+val client = KMongo.createClient(mongoDBconnectionURL)
 val database: MongoDatabase = client.getDatabase("solfibot")
--- src/main/kotlin/settings/db.kt
+++ src/main/kotlin/settings/db.kt
@@ -1,15 +1,10 @@
 package settings
 
 
-val jdbcURL = run {
-    val isContainer = System.getenv().getOrDefault("HOSTNAME", "") != ""
-    if (isContainer)
-        "db/db"
-    else
-        System.getenv("DB_PATH") ?: "db/db"
-}
+val mongoDBconnectionURL =
+        System.getenv("DB_URL") ?: "mongodb://localhost"
 
 @Suppress("SpellCheckingInspection") // because it is just a random value
 const val internalHiddenValue =
-    "Pq9J7tua26U7cxqrdJHzMC5ZJBoUUovgth65HGMpU0ze8agO2wPyLeUGaCSVnMW1KpqCOgXiE4Q51Y1WO4XYlUNaM7sfw03KGaKT"
+        "Pq9J7tua26U7cxqrdJHzMC5ZJBoUUovgth65HGMpU0ze8agO2wPyLeUGaCSVnMW1KpqCOgXiE4Q51Y1WO4XYlUNaM7sfw03KGaKT"
 
