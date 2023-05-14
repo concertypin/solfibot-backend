3297e16e95729b5be58bebb046d5790a041130c8
concertypin <55056558+concertypin@users.noreply.github.com>
2023-05-14 17:03:47 +0900

    I misconfigured

---
+++ src/main/kotlin/models/db/userData/userData.kt
@@ -0,0 +1,24 @@
+package models.db.userData
+
+import kotlinx.serialization.Serializable
+import settings.internalHiddenValue
+import settings.maxChance
+
+@Serializable
+data class UserData(val userid: String, val streamerData: StreamerData, val listenerData: ListenerData)
+
+@Serializable
+data class StreamerData(
+    val command: MutableMap<String, List<String>> =
+        mutableMapOf("?ping" to listOf("pong?"), internalHiddenValue to listOf(internalHiddenValue)),
+    var isSafeBrowsingEnabled: Boolean = false
+)
+
+@Serializable
+data class Roulette(var chances: Int = maxChance, var combo: Int = 0, var lastEditedTime: Long = 0)
+
+@Serializable
+data class ListenerData(
+    val score: MutableMap<String, Int> = mutableMapOf("0" to 0),
+    val roulette: MutableMap<String, Roulette> = mutableMapOf("0" to Roulette())
+)
---
+++ src/main/kotlin/models/db/userData/userDataModify.kt
@@ -0,0 +1,34 @@
+package models.db.userData
+
+class ModifyValueConflict : Exception("offset or data must be null.")
+
+data class Modify(val offset: Int? = null, val data: Int? = null)
+
+fun ListenerData.editRoulette(uid: String, chances: Modify = 0.offset, combo: Modify = 0.offset): ListenerData {
+    val rouletteData = roulette[uid] ?: Roulette(lastEditedTime = 0)
+    
+    if ((chances.data ?: rouletteData.chances) < rouletteData.chances || (chances.offset
+            ?: 1) < 0
+    ) //modified chance < original chance
+        rouletteData.lastEditedTime = System.currentTimeMillis()
+    if (chances.offset == null && chances.data != null)
+        rouletteData.chances = chances.data
+    else if (chances.offset != null && chances.data == null)
+        rouletteData.chances += chances.offset
+    else
+        throw ModifyValueConflict()
+    
+    if (combo.offset == null && combo.data != null)
+        rouletteData.combo = combo.data
+    else if (combo.offset != null && combo.data == null)
+        rouletteData.combo += combo.offset
+    else
+        throw ModifyValueConflict()
+    roulette[uid] = rouletteData
+    return this
+}
+
+val Int.offset: Modify
+    get() = Modify(this)
+val Int.data: Modify
+    get() = Modify(data = this)
